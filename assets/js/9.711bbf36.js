b983d16deef97832d865340f0101f0c5ef6ea2cf
George Meng <gqmeng.meng@gmail.com>
2019-04-17 11:36:31 -0400

    Updated ipp-demo to link "Open App" to "/home"

--- assets/js/9.711bbf36.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{167:function(n,t,e){"use strict";e.r(t);var s=e(0),c=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"content"})},[],!1,null,null,null);c.options.__file="cancer-advisor.md";t.default=c.exports}}]);
